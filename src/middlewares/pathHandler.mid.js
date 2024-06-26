b317ae54224340dc6e5f1c2f2e5c9fe6dc91f6ee
Santiago Jimenez <asjimenezg1923@gmail.com>
2024-06-25 19:26:31 -0500

    middle Fix

--- src/middlewares/pathHandler.mid.js
+++ src/middlewares/pathHandler.mid.js
@@ -1,7 +1,7 @@
 const pathHandler = (req, res, next) => {
   return res.json({
     statusCode: 404,
-    message: `${req.method} ${req.url} not found path`,
+    message: `${req.method} ${req.url} not found path!`,
   });
 };
 export default pathHandler;
